281ef29de4580c425f260ad688fa4b76662612a3
Jonak <jonakp.thakuria@gmail.com>
2025-12-30 21:31:15 +0530

    footer err

--- my-app/UI/univ/footer.tsx
+++ my-app/UI/univ/footer.tsx
@@ -1,6 +1,6 @@
 import {sec} from "@/UI/univ/font"
 
-export default function Foooter(color:string){
+export default function Foooter(){
     return(
         <div className="w-full h-max flex justify-center items-center bg-[var(--color-sec)]">
             <p className={`font-bold my-2 text-[0.8rem] ${sec.className} text-[var(--color-pri)]`}>©JonakThakuria</p>
