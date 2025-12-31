93af243ee8db78e967cfc50006f3b910246240b2
Jonak <jonakp.thakuria@gmail.com>
2025-12-31 16:50:54 +0530

    Dev

---
+++ my-app/UI/univ/dev.tsx
@@ -0,0 +1,9 @@
+export default function Devlopment(){
+    return(
+        <div className="w-full h-[800px] flex justify-center items-center">
+            <div className="w-max h-max bg-amber-50 border-4 p-5 border-red-800">
+                <h1 className="text-red-800 text-4xl font-black">Project Is under Development!</h1>
+            </div>
+        </div>
+    )
+}
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,14 @@
 import Main from "@/UI/home/home"
 import Nav from "@/UI/univ/nav"
 import Footer from "@/UI/univ/footer"
+import Dev from "@/UI/univ/dev"
 
 export default function Home() {
   return (
     <div className="bg-amber-50 text-black">
       <Nav/>
-      <Main/>
+      {/* <Main/> */}
+      <Dev/>
       <Footer/>
     </div>
   );
